54e3003e66cd1e0f853ee46f985abf602c3631b2
Nguyen Thanh Dat <datnguyen9240@gmail.com>
2026-06-02 20:29:20 +0700

    style: Combine separate INSERT statements into a single multi-value INSERT in Update_WA_API_Booking.sql

--- sql/Query/Update_WA_API_Booking.sql
+++ sql/Query/Update_WA_API_Booking.sql
@@ -14,37 +14,28 @@ UPDATE SY_FormatFields
 SET FieldName = 'DiaChi' 
 WHERE FormName = 'frmBiennhancoccho' AND FieldName = 'Diachi';
 
--- 2. Cấu hình định tuyến cho Danh sách Đặt cọc trên giao diện chính (frmBiennhancoccho)
+-- 2. Cấu hình định tuyến cho Danh sách Đặt cọc (frmBiennhancoccho) và các mẫu in (frmBiennhancocchoancoccho, frmPhieuThu)
 INSERT INTO WA_API (List, Func, [SQL], Para)
-VALUES (
+VALUES 
+(
     'frmBiennhancoccho', 
     'View', 
     'API_DanhSachPhieuCoc', 
     '@TuNgay=N''{TuNgay}'', @DenNgay=N''{DenNgay}'', @Keyword=N''{Keyword}'''
-);
-
-INSERT INTO WA_API (List, Func, [SQL], Para)
-VALUES (
+),
+(
     'frmBiennhancoccho', 
     'Save', 
     'API_LuuPhieuCoc', 
     '@DocumentID=N''{DocumentID}'', @Tenchure=N''{Tenchure}'', @Tencodau=N''{Tencodau}'', @DTchure=N''{DTchure}'', @DTcodau=N''{DTcodau}'', @Diachi=N''{DiaChi}{Diachi}'', @Nguoigd=N''{Nguoigd}'', @DienThoaiDaiDien=N''{DienThoaiDaiDien}'', @Mail=N''{Mail}'', @Ngaytochuc=N''{Ngaytochuc}'', @Loaitiecid=N''{Loaitiecid}'', @Thoigianid=N''{Thoigianid}'', @SobanManchinhthuc=N''{SobanManchinhthuc}'', @SobanManduphong=N''{SobanManduphong}'', @SobanChaychinhthuc=N''{SobanChaychinhthuc}'', @SobanChayduphong=N''{SobanChayduphong}'', @Tongtien=N''{Tongtien}'', @Solan=N''{Solan}'', @Ghichu=N''{Ghichu}'', @JsonSanhTiec=N''{JsonSanhTiec}'''
-);
-
--- 3. Cấu hình định tuyến fallback cho in ấn mẫu Đặt cọc (frmBiennhancocchoancoccho)
--- (Do server.js định nghĩa loại mẫu 'dat_coc' map với list 'frmBiennhancocchoancoccho')
-INSERT INTO WA_API (List, Func, [SQL], Para)
-VALUES (
+),
+(
     'frmBiennhancocchoancoccho', 
     'View', 
     'API_DanhSachPhieuCoc', 
     '@Keyword=N''{Keyword}'''
-);
-
--- 4. Cấu hình định tuyến fallback cho in ấn mẫu Phiếu thu (frmPhieuThu)
--- (Do server.js định nghĩa loại mẫu 'phieu_thu' map với list 'frmPhieuThu')
-INSERT INTO WA_API (List, Func, [SQL], Para)
-VALUES (
+),
+(
     'frmPhieuThu', 
     'View', 
     'API_DanhSachPhieuCoc', 
