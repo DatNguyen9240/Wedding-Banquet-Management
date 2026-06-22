751cda0a9a86809cafca0a42b5e07146305c826b
Nguyen Thanh Dat <datnguyen9240@gmail.com>
2026-06-22 14:50:48 +0700

    feat: add DocumentExportPlugin to handle automated document generation across multiple management forms

--- src/js/utils/DocumentExportPlugin.js
+++ src/js/utils/DocumentExportPlugin.js
@@ -232,7 +232,7 @@ var DocumentExportPlugin = (function () {
             altKeys: ['Sohopdong', 'sohopdong', 'SoHopDong'],
             sqlListName: 'API_DanhSachPhatSinh',
             ignoreTemplateFile: true,
-            convertFields: ['MenuPhatSinh']
+            convertFields: []
           });
         }
       });
